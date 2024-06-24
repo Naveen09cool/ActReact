d358e5f2163dbbf49093fbcb05c8e3039a91ac73
Naveen Singh <naveen09cool@gmail.com>
2024-06-24 16:08:25 +0530

    CSS, Nested Divs

--- chapter_01/part3/App.js
+++ chapter_01/part3/App.js
@@ -2,4 +2,19 @@ const heading = React.createElement("h1", {}, "Hello from React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id:"child1"},[
+        React.createElement("h1", {}, "First Child"),
+        React.createElement("h2", {}, "Second Child")
+    ]), 
+    React.createElement("div", {id:"child2"},[
+        React.createElement("h1", {}, "First Child"),
+        React.createElement("h2", {}, "Second Child")
+    ])
+])
+
+root.render([heading,parent]);
+
+// Usually JSX is used in react instead of above defined way as this becomes too complicated to read when structure gets big.
